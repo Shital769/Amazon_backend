d625a4eee83d00994a29529671f4ada3878986cf
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-10-08 19:48:57 +1100

    created place order api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -33,6 +34,7 @@ mongoose
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("Server is Ready"); // send response to client
